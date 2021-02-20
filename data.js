54abc7c96efe04b0a00fe91058627499a9d07c04
begimai-imamidin <76731581+begimai-imamidin@users.noreply.github.com>
2021-02-20 13:28:21 +0600

    uploaded files

---
+++ data.js
@@ -0,0 +1,39 @@
+const menu_items = [{
+        id: 1,
+        title: 'Hamburger',
+        price: 80,
+        img: './images/hamburger.png'
+    },
+    {
+        id: 2,
+        title: 'Coffee',
+        price: 100,
+        img: './images/coffee.png'
+    },
+    {
+        id: 3,
+        title: 'Cola',
+        price: 60,
+        img: './images/cola.svg'
+    },
+    {
+        id: 4,
+        title: 'Tea',
+        price: 50,
+        img: './images/tea.svg'
+    },
+    {
+        id: 5,
+        title: 'Cheeseburger',
+        price: 100,
+        img: './images/cheeseburger.svg'
+    },
+    {
+        id: 6,
+        title: 'fries',
+        price: 80,
+        img: './images/fries.svg'
+    },
+]
+
+let orders_basket = []
